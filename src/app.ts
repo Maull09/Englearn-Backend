76242ef9e269bb68eeb3ac5e50f62cb716f067b1
Maull09 <maulanafirdaus63@gmail.com>
2025-01-17 18:46:03 +0700

    deact security

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,11 @@ app.set("trust proxy", 1);
 // Configure CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000" || "http://localhost:8081",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8081",
+      "https://englearnuniversal.vercel.app",
+    ],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization,user-id",
     credentials: true, // Allow credentials (cookies)
